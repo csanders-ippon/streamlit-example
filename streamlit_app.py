bf6b05f98d999959f3a5543e8b5d86284402b9ff
csanders-ippon <111074378+csanders-ippon@users.noreply.github.com>
2023-06-05 22:23:14 +0000

    test 5

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 """
-# Random text here!!
+# How quickly does it update?
 """
 
 with st.echo(code_location='below'):
